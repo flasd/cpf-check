e097c21ba02d26bc746569b745a25e18ca6ef169
Marcel Coelho <marcel.coelho@coopersystem.com.br>
2018-11-12 14:26:14 -0200

    style: add linting rules for test files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,20 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "rules": {
-        "indent": ["warn", 4]
+  parser: "babel-eslint",
+  extends: "airbnb-base",
+  rules: {
+    indent: ["warn", 4]
+  },
+  overrides: [
+    {
+      files: ["src/**/*.test.js"],
+      rules: {
+        "import/no-extraneous-dependencies": "off",
+        "global-require": "off"
+      },
+      env: {
+        browser: false,
+        jest: true
+      }
     }
+  ]
 };
